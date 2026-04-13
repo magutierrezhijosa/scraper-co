7ef4ce3f55921afbe19164be08b2bdc7a80642ce
Miguel Angel Gutiérrez Hijosa <magutierrezhijosa@gmail.com>
2026-04-13 17:36:21 +0200

    Cambios en el scraper.py

--- scraper.py
+++ scraper.py
@@ -2,46 +2,16 @@
 ###### IMPORTS ######
 
 from playwright.sync_api import sync_playwright
+from bs4 import BeautifulSoup
+import pandas as pd
 
 
-# Creamos una funcion para abrirr la pagina y ver el HTML que nos devuelve
-def explorara_pagina():
-    # Creamos el contexto donde vamos a abrir el navegador y una vez se ejecute todo el codigo se cerrara automaticamente
-    with sync_playwright() as p:
 
-        # Abrimos el navegador Chronium visible (headless=False)
-        navegador = p.chromium.launch(headless=False)
-
-        # Creamos una nueva ventana de navegacion
-        pagina = navegador.new_page()
-
-        # Le decimos la pagina a la que va a navegar
-        print("🌐 Abriendo la página...")
-        pagina.goto("https://www.dgeec.medu.pt/destaques")
-
-        # Esperemos a que cargue el contenido principal hasta que la pagina deje de hacer peticiones de red (networkidle)
-        pagina.wait_for_load_state("networkidle")
-        print("✅ Página cargada!")
-
-        # Guardamos el HTML para inspeccionarlo 
-        html = pagina.content()
-
-        # Creamos otro contexto para abrir un archivo y guardar el contenido de la web en este
-        with open("pagina.html" , "w" , encoding="utf-8") as file_html:
-            
-            # Utilizamos la funcion para escribir en nuestro fichero
-            file_html.write(html)
-        
-        print("💾 HTML guardado en pagina.html")
-
-        input("Pulsa ENTER para cerrar el navegdor")
-
-        # Cerramos el navegador
-        navegador.close()
+######  BLOQUE 1 - Recoger publicaciones de la pagina principal #######
+def obtener_publicaciones(pagina):
 
 
-# Ejecutamos la funcion 
-explorara_pagina()
 
+    return 
 
 
