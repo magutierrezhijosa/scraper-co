05b8ab6ecd637b32d1c5f19cbc2d0e95aec056bd
Miguel Angel Gutiérrez Hijosa <magutierrezhijosa@gmail.com>
2026-04-14 17:13:52 +0200

    config.py terminado

---
+++ config.py
@@ -0,0 +1,14 @@
+############### CONFIGURACION GLOBAL DEL SCRAPER ################
+
+# URL base de la web que vamos a scrapear
+BASE_URL = "https://www.dgeec.medu.pt"
+
+# URL de la pagina principal destaques
+URL_DESTAQUES = BASE_URL + "/destaques"
+
+# Nombre del archivo CSV donde guardaremos los resultados
+NOMBRE_CSV = "resultados.csv"
+
+# Selector CSS del elemento padre de cada publicacion
+SELECTOR_PUBLICACIONES = "span.grelha-item"
+
