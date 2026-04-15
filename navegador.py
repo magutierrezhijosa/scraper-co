847a744d1131d7d5a566d8db2e6b85a58ae24364
Miguel Angel Gutiérrez Hijosa <magutierrezhijosa@gmail.com>
2026-04-15 20:42:36 +0200

    Funciuon cagar_pagina_principal() definida

--- navegador.py
+++ navegador.py
@@ -22,8 +22,25 @@ def crear_navegador():
 
 # Funcion encargada de cerrar el navegador y el contexto
 def cerrar_navegador(playright,navegador):
+    
     """Cierra el navegador y Playwright correctamente"""
     navegador.close()
     playright.stop()
 
 
+# Funcion que se encarga  de ir a la pagina principal y esperar a que se carguen todo el contenido dinamico de la pagina , hasta poder recoger los selectores 
+def cargar_pagina_principal(pagina):
+
+    """Navega a la pagina principal y espera a que cargue"""
+    print("🌐 Abriendo la página principal...")
+
+    # Vamos a la pagina que deseamos
+    pagina.goto(URL_DESTAQUES)
+    # Esperamos a que se cargue el contenido dinamico de la pagina s
+    pagina.wait_for_load_state("networkidle")
+    # Esperamos a que se carguen los selectores de las publicaciones
+    pagina.wait_for_selector(SELECTOR_PUBLICACIONES)
+    print("✅ Página principal cargada correctamente.")
+
+
+# Funcion que se encaga de navegar a una subpagina y 
