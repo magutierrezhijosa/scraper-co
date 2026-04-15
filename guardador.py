8a50e1edec29dae057611f60b1566ccb315d9944
Miguel Angel Gutiérrez Hijosa <magutierrezhijosa@gmail.com>
2026-04-15 20:51:27 +0200

    Creamos guardador.py

---
+++ guardador.py
@@ -0,0 +1,10 @@
+
+############# GUARDADOR - Guardar resultados en CSV ###############
+
+import pandas as pd
+from config import NOMBRE_CSV
+
+
+# Declaramos la funcion que se encarga de guardar los resultados en un archivo CSV
+def guardar_csv(resultados):
+    
