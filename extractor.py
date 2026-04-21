ba3852e65f1661be02848c9ff9592d22f5d0a0c3
Miguel Angel Gutiérrez Hijosa <magutierrezhijosa@gmail.com>
2026-04-21 13:27:08 +0200

    Funcion extraer_pdfs()

--- extractor.py
+++ extractor.py
@@ -19,4 +19,55 @@ def obtener_publicaciones(pagina):
     # Mostramos en pantala los resultados encontrados 
     print(f"🔍 Encontrados {len(items)} publicaciones")
 
-    
+    # Creamos una lista para almacenar los resultados
+    publicaciones = []
+
+    # Recorremos cada elemento encontrado y extraemos el titulo y el enlace
+    for item in items:
+
+        # Buscamos el enlace dentro del elemento usando el selector CSS
+        enlace = item.find("a", class_="glelha-item-titulo")
+
+        if enlace:
+
+            # Obtenemos el titulo del enlace y lo limpiamos de espacios en blanco
+            titulo = enlace.get_text(strip=True)
+
+            # Obtenemos el enlace del elemento
+            href = enlace.get("href")
+
+            # Construimos la URL completa de la publicación
+            url_completa = BASE_URL + href
+            
+            # Agregamos el titulo y el enlace a la lista de publicaciones
+            publicaciones.append({
+                "titulo": titulo, 
+                "enlace": url_completa
+            })
+
+    return publicaciones
+
+
+def extraer_pdfs(pagina):
+    """Busca todos los enlaces a PDFs en la pagina actual """
+
+    # Obtenemos el contenido HTML de la pagina 
+    html = pagina.content()
+
+    # Analizamos el HTML con BeautifulSoup 
+    soup = BeautifulSoup(html, "html.parser")
+
+    # Creamos na tupla para almacenar los enlaces a PDFs encontrados
+    pdfs = []
+
+    # Buscamos todos los enlaces en la pagina
+    for enlace in soup.find_all("a", href=True):
+
+        # Recogemos el enlace 
+        href = enlace.get("href")
+
+        # Verificamos si el enlace termina con .pdf (indicando que es un PDF)
+        if href.lower().endswith(".pdf"):
+
+            # Construimos la URL completa si es  relativa 
+            
