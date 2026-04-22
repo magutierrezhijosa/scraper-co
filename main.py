6f6715cffebc76e077072831eac362e789797d07
Miguel Angel Gutiérrez Hijosa <magutierrezhijosa@gmail.com>
2026-04-22 20:46:07 +0200

    Creamos la funcion main() y empezamos a montar toda la funcion principal

--- main.py
+++ main.py
@@ -5,5 +5,14 @@ from extractor import obtener_publicaciones, extraer_pdfs
 from guardador import guardar_csv
 
 # Declaramos la funcion principal del programa
+def main():
 
+    # Iniciamos el navegador y recogemos las variables empaquetadas que nos devuelve la funcion crear_navegador()
+    playwright, navegador , pagina = crear_navegador()
 
+    try:
+        # Cargamos la pagina principal
+        cargar_pagina_principal(pagina)
+
+        # Obtenemos las publicaciones de la pagina principal
+        publicaciones = obtener_publicaciones(pagina)
